4166809062234201fcdb747338b5deafeb6fc5c7
Luis Jimenez <lu.jim@protonmail.com>
2025-09-27 12:48:47 +0200

    feat - add company founder and agent models

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ApplicationRecord
+  has_many :founders, dependent: :destroy
+  accepts_nested_attributes_for :founders, allow_destroy: true, reject_if: :all_blank
+  
+  validates :name, presence: true
 end
